aa884db26a7158bc047487ec728b442fa9b4f5b8
ayumitamai <ayumitamai97@gmail.com>
2018-05-17 22:19:05 +0900

    enabled access from outside the univ

--- maisaku.rb
+++ maisaku.rb
@@ -11,8 +11,8 @@ Capybara.current_driver = :poltergeist
 Capybara.configure do |config|
   config.run_server = false
   config.javascript_driver = :poltergeist
-  config.app_host = "https://dbs.g-search.or.jp" # 学内
-  # config.app_host = "http://database.asahi.com.ez.wul.waseda.ac.jp" # 学外
+  # config.app_host = "https://dbs.g-search.or.jp" # 学内
+  config.app_host = "https://dbs-g-search-or-jp.ez.wul.waseda.ac.jp" # 学外
   config.default_max_wait_time = 30
   config.ignore_hidden_elements = false
 end
@@ -35,33 +35,30 @@ def login_inside_univ
 end
 
 def login_outside_univ
-  # visit "http://www.wul.waseda.ac.jp.ez.wul.waseda.ac.jp/DOMEST/db_about/dna/dna.html"
-  # fill_in "user", :with => ARGV[0]
-  # fill_in "pass", :with => ARGV[1]
-  # all("input")[3].trigger("click")
-  # sleep(5)
-  # visit find("a.A_button")[:href]
-  # sleep(5)
-  # all("#contentMain a")[0].trigger("click")
-  # sleep(10)
+  visit "http://www.wul.waseda.ac.jp.ez.wul.waseda.ac.jp/DOMEST/db_about/maisaku/maisaku.html"
+  fill_in "user", :with => ARGV[0]
+  fill_in "pass", :with => ARGV[1]
+  all("input")[3].trigger("click")
+  sleep(5)
+  visit find("a.A_button")[:href]
+  sleep(5)
 end
 
 def search
   visit "/aps/WMSK/main.jsp?uji.verb=GSHWA0300&serviceid=WMSK"
   sleep(15)
-  binding.pry
   all("tr.middle label")[1].trigger("click")
   all("tr.middle label")[72].trigger("click")
   fill_in("paraTi", with: "訪日 AND 中国人")
   find("select#paraYearFrom").find("option[value='2015']").select_option
   find("select#paraMonthFrom").find("option[value='1']").select_option
   find("select#paraDayFrom").find("option[value='1']").select_option
-  all("select")[4].find("option[value='2']").select_option
+  all("select")[4].find("option[value='2']").select_option # 「から」を選択
   find("select#paraYearTo").find("option[value='2015']").select_option
   find("select#paraMonthTo").find("option[value='12']").select_option
   find("select#paraDayTo").find("option[value='31']").select_option
-  # all("input.btext")[0].trigger("click")
-  save_screenshot "1.png"
+  all("input")[15].trigger("click")
+  # binding.pry
   sleep(5)
 end
 
@@ -106,7 +103,7 @@ def get_search_result
   # end
 end
 
-# login_outside_univ
-login_inside_univ
+login_outside_univ
+# login_inside_univ
 search
 get_search_result
